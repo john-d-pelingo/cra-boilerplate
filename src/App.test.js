9b7eb44d888fe09dc32d38050bcc3cb6181a3955
John Darryl Pelingo <johndarrylpelingo@gmail.com>
2018-04-20 17:50:45 +0300

    Various changes
    - Add more tests
    - Add scripts
    - Remove unnecessary dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -10,15 +10,28 @@ describe('Default suite', () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it('should be selectable by class "App"', function() {
+  it('should be selectable by class "App"', () => {
     expect(shallow(<App />).is('.App')).toBe(true);
   });
 
-  it('should mount in a full DOM', function() {
+  it('should mount in a full DOM', () => {
     expect(mount(<App />).find('.App').length).toBe(1);
   });
 
-  it('should render to static HTML', function() {
+  it('should render to static HTML', () => {
     expect(render(<App />).text()).toContain('Welcome to React');
+  });
+
+  it('should render two children', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.children().length).toBe(2);
+  });
+
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<App />);
+    const snapJson = enzymeToJson(wrapper);
+
+    expect(snapJson).toMatchSnapshot();
   });
 });
