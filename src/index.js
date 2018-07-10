acc917e26eb8da4a794a754b6d9655320f9d0179
John Darryl Pelingo <johndarrylpelingo@gmail.com>
2018-07-10 11:10:47 +0300

    Fix linting error

--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+// eslint-disable-next-line no-unused-expressions
 module.hot && module.hot.accept();
