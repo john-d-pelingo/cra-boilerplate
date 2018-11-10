cf390f2ee45592e8bc7d5c73ccd84d31418c7422
John Darryl Pelingo <johndarrylpelingo@gmail.com>
2018-11-10 23:34:52 +0100

    Add gold goodies (read more)
    - add CSS modules with SCSS
    - use detailed structure for components
    - move reset CSS to assets

---
+++ src/components/app/app.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+import logo from 'src/assets/logo.svg'
+import styles from './styles.module.scss'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        <header className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <h1 className={styles.appTitle}>Welcome to React</h1>
+        </header>
+        <p className={styles.appIntro}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
