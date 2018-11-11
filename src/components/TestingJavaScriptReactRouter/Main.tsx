11b35e2698eed73efae2aa47005e97f37335660a
John Darryl Pelingo <johndarrylpelingo@gmail.com>
2018-11-12 00:04:30 +0100

    Add react-router-dom testing

---
+++ src/components/TestingJavaScriptReactRouter/Main.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { Link, Route, Switch } from 'react-router-dom'
+
+const About = () => (
+  <div data-testid="about-screen">You are on the about page</div>
+)
+const Home = () => <div data-testid="home-screen">You are home</div>
+const NoMatch = () => <div data-testid="no-match-screen">No match</div>
+
+const Main: React.FunctionComponent = () => (
+  <div>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route component={NoMatch} />
+    </Switch>
+  </div>
+)
+
+export default Main
