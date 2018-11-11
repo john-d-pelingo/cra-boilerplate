37d8c93cbc9f93e65123f32b61dd59a93f6e50c1
John Darryl Pelingo <johndarrylpelingo@gmail.com>
2018-11-11 03:07:45 +0100

    Fix issues (read more)
    - update README
    - fix annoying case error
    - add FavoriteNumber test

--- src/components/App/__tests__/App2.test.tsx
+++ src/components/App/__tests__/App2.test.tsx
@@ -3,9 +3,9 @@ import 'jest-dom/extend-expect'
 import * as React from 'react'
 import { render } from 'react-testing-library'
 
-import App from '../app'
+import App from '../App'
 
-describe('components/app', () => {
+describe('components/App', () => {
   it('renders an image', () => {
     const { getByAltText } = render(<App />)
     const image = getByAltText('App Logo')
