488f20e6ff68f6230fc3812bf078bc835b07e692
John Darryl Pelingo <johndarrylpelingo@gmail.com>
2018-11-14 22:16:58 +0100

    Add react unmount tests

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import logo from 'src/assets/logo.svg'
 
 import {
+  Countdown,
   Editor,
   ErrorBoundary,
   FavoriteNumber,
   GreetingLoaderDependencyInjection,
   GreetingLoaderHttpJestMock,
   HiddenMessage,
+  Modal,
   Toggle,
 } from '../'
 import styles from './styles.module.scss'
@@ -24,15 +26,34 @@ class App extends React.Component {
           To get started, edit <code>src/components/app/app.tsx</code> and save
           to reload.
         </p>
+        <br />
+        <br />
 
+        <h1>Basic</h1>
         <br />
         <FavoriteNumber />
         <br />
+        <br />
+
+        <h1>Mock</h1>
+        <br />
         <GreetingLoaderHttpJestMock />
+        <br />
+        <br />
+
+        <h1>Dependency Injection</h1>
         <br />
         <GreetingLoaderDependencyInjection />
         <br />
+        <br />
+
+        <h1>React Transition Group</h1>
+        <br />
         <HiddenMessage>Hello</HiddenMessage>
+        <br />
+        <br />
+
+        <h1>Error Boundary</h1>
         <br />
         <ErrorBoundary>
           {(({ shouldThrow = true }: { shouldThrow?: boolean }) => {
@@ -46,14 +67,34 @@ class App extends React.Component {
           })({ shouldThrow: false })}
         </ErrorBoundary>
         <br />
-        {/* Will fail because Redirect is not inside Router */}
+        <br />
+
+        <h1>React Router</h1>
+        <br />
+        {/* Submitting will fail because Redirect is not inside Router */}
         <Editor user={{ id: '1' }} />
+        <br />
+        <br />
+
+        <h1>Render Prop</h1>
         <br />
         <Toggle>
           {({ on, toggle }) => (
             <button onClick={toggle}>{on ? 'On' : 'Off'}</button>
           )}
         </Toggle>
+        <br />
+        <br />
+
+        <h1>React Modal</h1>
+        <br />
+        <Modal>Modal boyz</Modal>
+        <br />
+        <br />
+
+        <h1>React Unmount</h1>
+        <br />
+        <Countdown />
       </div>
     )
   }
